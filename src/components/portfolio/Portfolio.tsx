8478cf83de59d8eafd7ddbb972b0403cedf9875c
LevkovichV <slavlen1999@gmail.com>
2021-09-26 15:16:39 +0300

    dev: created and added loader to portfolio page

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
 import { services } from '../../services/services';
+import Loader from '../utils/loader/Loader';
 import './portfolio.scss';
 import PortfolioItem from './portfolioItem/PortfolioItem';
 import PortfolioPage from './portfolioPages/PortfolioPage';
 
 const Portfolio: FC = () => {
+  const [loading, setLoading] = useState(false);
   const [selected, setSelected] = useState(0);
   const [data, setData] = useState<IPortfolio[]>([]);
   const [pagesCount, setPagesCount] = useState(1);
@@ -14,39 +16,46 @@ const Portfolio: FC = () => {
     const startIdx = selected * amountPerPage;
     const endIdx = startIdx + amountPerPage;
 
+    setLoading(true);
+
     services.portfolioService
       .getPortfolioList()
       .then((data) => {
         setData(data.slice(startIdx, endIdx));
         setPagesCount(Math.floor(data.length / amountPerPage));
       })
-      .catch((e) => console.error(e));
+      .catch((e) => console.error(e))
+      .finally(() => setLoading(false));
   }, [selected]);
 
   return (
     <div className="portfolio" id="portfolio">
-      <h1>Portfolio</h1>
-
-      <ul>
-        {new Array(pagesCount).fill(1).map((_, idx) => (
-          <PortfolioPage
-            key={idx}
-            idx={idx}
-            active={selected === idx}
-            setActive={setSelected}
-          />
-        ))}
-      </ul>
-
-      <div className="container">
-        {data.map((item) => (
-          <PortfolioItem
-            key={item.id}
-            imgSrc={item.imgSrc}
-            title={item.title}
-          />
-        ))}
-      </div>
+      {!loading ? (
+        <>
+          <h1>Portfolio</h1>
+          <ul>
+            {new Array(pagesCount).fill(1).map((_, idx) => (
+              <PortfolioPage
+                key={idx}
+                idx={idx}
+                active={selected === idx}
+                setActive={setSelected}
+              />
+            ))}
+          </ul>
+          <div className="container">
+            {data.map((item) => (
+              <PortfolioItem
+                key={item.id}
+                imgSrc={item.imgSrc}
+                title={item.title}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
---
+++ src/components/utils/loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './loader.scss';
+
+const Loader: FC = () => (
+  <div className="loader-wrapper">
+    <div className="loader" />
+  </div>
+);
+
+export default Loader;
