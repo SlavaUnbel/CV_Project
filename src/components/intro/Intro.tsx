a10b4af5f2c49467380e28e99c1680534092f5a3
LevkovichV <slavlen1999@gmail.com>
2021-09-26 10:03:11 +0300

    dev: created intro page and applied styles to it

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,8 +1,51 @@
-import React, { FC } from 'react';
+import { init } from 'ityped';
+import React, { createRef, FC, LegacyRef, useEffect } from 'react';
+import { avatarSrc, downSrc } from '../../utils/constants';
+import { SECOND } from '../../utils/date';
 import './intro.scss';
 
 const Intro: FC = () => {
-  return <div className="intro" id="home"></div>;
+  const textRef: LegacyRef<HTMLSpanElement> = createRef();
+
+  useEffect(() => {
+    textRef.current &&
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: SECOND * 2,
+        strings: [
+          'Front-End Developer',
+          'React SPA Creator',
+          'Inspired Programmer',
+        ],
+      });
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="intro" id="home">
+      <div className="left">
+        <div className="img-container">
+          <img src={avatarSrc} alt="Avatar" />
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+
+          <h1>Slava Levkovich</h1>
+
+          <h3>
+            <span ref={textRef} />
+          </h3>
+        </div>
+
+        <a href="#portfolio">
+          <img src={downSrc} alt="" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
