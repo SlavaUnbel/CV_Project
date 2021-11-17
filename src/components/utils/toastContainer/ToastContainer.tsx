2c3b059622de9868e375e0feb034fb55ae8659a6
LevkovichV <slavlen1999@gmail.com>
2021-11-17 16:22:44 +0300

    fix: quick audio player fix

--- src/components/utils/toastContainer/ToastContainer.tsx
+++ src/components/utils/toastContainer/ToastContainer.tsx
@@ -1,21 +1,25 @@
 import React, { FC } from 'react';
-import { ToastContainer as ToastContainerUI } from 'react-toastify';
+import {
+  ToastContainer as ToastContainerUI,
+  ToastPosition,
+} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
   autoClose?: number | false;
   limit?: number;
+  position?: ToastPosition;
   onClick?: () => void;
 }
 
-const ToastContainer: FC<Props> = ({ autoClose, limit, onClick }) => (
+const ToastContainer: FC<Props> = ({ autoClose, limit, position, onClick }) => (
   <ToastContainerUI
     onClick={onClick}
     closeButton={false}
     theme="colored"
     limit={limit || 3}
     autoClose={autoClose}
-    position="bottom-right"
+    position={position || 'top-right'}
   />
 );
 
